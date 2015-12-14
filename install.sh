c4a9e4355eceb8ccb6a1fe948be33a8e5619c1e9
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2015-12-14 17:15:26 +0100

    Brought back previous path from installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf C:/biserver-ce/pentaho-solutions/system/nms-ldap-manager
-unzip target/nms-ldap-manager-plugin.zip -d C:/biserver-ce/pentaho-solutions/system
+rm -rf ~/biserver-ce/pentaho-solutions/system/nms-ldap-manager
+unzip target/nms-ldap-manager-plugin.zip -d ~/biserver-ce/pentaho-solutions/system
