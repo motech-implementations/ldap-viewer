025acede02ac438e4058a318f17e7c8fd55ed792
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2016-02-01 17:05:31 +0100

    NIP-352 Fixed the display of a single user in table

--- src/main/java/org/motechproject/nms/ldapbrowser/ldap/web/LdapAjaxService.java
+++ src/main/java/org/motechproject/nms/ldapbrowser/ldap/web/LdapAjaxService.java
@@ -1,5 +1,6 @@
 package org.motechproject.nms.ldapbrowser.ldap.web;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.motechproject.nms.ldapbrowser.ldap.DistrictInfo;
 import org.motechproject.nms.ldapbrowser.ldap.LdapUser;
 import org.motechproject.nms.ldapbrowser.ldap.LdapUserService;
@@ -8,6 +9,8 @@ import org.motechproject.nms.ldapbrowser.region.RegionService;
 import org.motechproject.nms.ldapbrowser.support.web.DtData;
 import org.motechproject.nms.ldapbrowser.support.web.DtRequest;
 import org.pentaho.platform.web.http.api.resources.JaxbList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,10 +20,13 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.util.List;
 
 @Path("/nms-users/api")
 public class LdapAjaxService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LdapAjaxService.class);
 
     private LdapUserService ldapUserService;
     private RegionService regionService;
@@ -29,13 +35,20 @@ public class LdapAjaxService {
     @Path("/users")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getUsers(DtRequest dtRequest) {
+    public String getUsers(DtRequest dtRequest) {
         UsersQuery query = new UsersQuery(dtRequest.getStart(), dtRequest.getLength());
 
         List<LdapUser> users = ldapUserService.getUsers(query);
         DtData<LdapUser> dtData = new DtData<>(users, users.size());
+        ObjectMapper mapper = new ObjectMapper();
+        String usersResponse = null;
+        try {
+            usersResponse = mapper.writeValueAsString(dtData);
+        } catch (IOException e) {
+            LOG.error("The serialization of the users object to JSON failed.", e);
+        }
 
-        return Response.ok(dtData).type(MediaType.APPLICATION_JSON).build();
+        return usersResponse;
     }
 
     @GET
